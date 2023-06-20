4a15489968b1d1c7b40f9a8613f17085873c372c
kjungw1025 <kjungw1025@naver.com>
2023-06-20 21:25:20 +0900

    feat: Create user.js

---
+++ learn-sequelize/models/user.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequlize');
+
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        User.init({
+            // id는 sequelize에서 자동으로 설정해줌
+            name: {
+                type: Sequelize.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+            age: {
+                type: Sequelize.INTEGER.UNSIGNED,
+                allowNull: false,
+            },
+            married: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+            comment: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            }
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id'});
+    }
+};
+
+module.exports = User;
