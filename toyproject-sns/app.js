c297c63c5f3f6e4d905610a64ab5854f96e407b2
kjungw1025 <kjungw1025@naver.com>
2023-06-27 15:47:04 +0900

    feat: Create app.js

---
+++ toyproject-sns/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const session = require('session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
