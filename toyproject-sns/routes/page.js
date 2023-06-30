8fe8926288754fdc74f61e53ea41c50ab33dd814
kjungw1025 <kjungw1025@naver.com>
2023-06-30 11:10:22 +0900

    feat: Update routes/page.js

--- toyproject-sns/routes/page.js
+++ toyproject-sns/routes/page.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
 const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
 
 const router = express.Router();
 
 // 라우터에서 공통적으로 쓸 수 있는 변수들을 res.locals를 통해 선언
 router.use((req, res, next) => {
-    res.locals.user = null;
+    res.locals.user = req.user;
     res.locals.followerCount = 0;
     res.locals.followingCount = 0;
     res.locals.followingIdList = [];
@@ -13,9 +14,9 @@ router.use((req, res, next) => {
 });
 
 // 라우터의 마지막 미들웨어들을 컨트롤러라고 부름 --> 따로 폴더(controllers)로 분리
-router.get('/profile', renderProfile);
+router.get('/profile', isLoggedIn, renderProfile);
 
-router.get('/join', renderJoin);
+router.get('/join', isNotLoggedIn, renderJoin);
 
 router.get('/', renderMain);
 
