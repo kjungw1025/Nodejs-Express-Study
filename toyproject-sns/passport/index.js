a6e730b966c4d3d6fa703c10c27eacb82750590c
kjungw1025 <kjungw1025@naver.com>
2023-07-03 11:17:44 +0900

    feat: Update passport/index.js
    Modified deserializeUser

--- toyproject-sns/passport/index.js
+++ toyproject-sns/passport/index.js
@@ -5,13 +5,29 @@ const User = require('../models/user');
 
 module.exports = () => {
     passport.serializeUser((user, done) => {    // user === exUser
+        console.log('serialize');
         done(null, user.id); // user id만 추출
     });
     // {세션 쿠키 : 유저 아이디} --> 메모리에 저장. 나중에는 공유되는 메모리에 저장
 
     passport.deserializeUser((id, done) => {
-        User.findOne({ where: { id } })
-            .then(user => done(null, user)) // req.user
+        console.log('deserialize');
+        User.findOne({ 
+            where: { id },
+            include: [{
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followers',
+            }, {
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followings',
+            }],
+        })
+            .then(user => {
+                console.log('user', user);
+                done(null, user)
+            }) // req.user
             .catch(err => done(err));
     });
 
