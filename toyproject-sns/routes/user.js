1b891efe36d851ab347584ddb3d9aa0a3e7484fd
kjungw1025 <kjungw1025@naver.com>
2023-07-03 13:02:26 +0900

    feat: Update routes/user.js
    Add unfollow router

--- toyproject-sns/routes/user.js
+++ toyproject-sns/routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const { isLoggedIn } = require('../middlewares');
-const { follow } = require('../controllers/user');
+const { follow, unfollow } = require('../controllers/user');
 
 const router = express.Router();
 
 // POST /user/:id/follow
 router.post('/:id/follow', isLoggedIn, follow);
+
+// POST /user/:id/unfollow
+router.post('/:id/unfollow', isLoggedIn, unfollow);
 
 module.exports = router;
