d8c508505704b48036bb86b8dc9e351bbebf465f
kjungw1025 <kjungw1025@naver.com>
2023-07-03 11:25:11 +0900

    feat: Update app.js
    Connected the following router to app.js

--- toyproject-sns/app.js
+++ toyproject-sns/app.js
@@ -13,6 +13,7 @@ dotenv.config(); // process.env
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -54,6 +55,7 @@ app.use(passport.session()); // connect.sid라는 이름으로 세션 쿠키가 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
